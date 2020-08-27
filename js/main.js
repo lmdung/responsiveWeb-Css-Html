42e32b0d5596bc7077d93d9cf68e7315d4f865be
leminhdung <lmdung94@gmail.com>
2020-08-27 14:40:36 +0700

    update main js

--- js/main.js
+++ js/main.js
@@ -1,5 +1,6 @@
 (() => {
   const mobileWidth = 680;
+
   const addMenuBackground = () => {
     const pageWidth = window.innerWidth; //Get the current frame's height and width
     const bodyOffset = document.body.scrollTop || document.documentElement.scrollTop // Safari || Chrome, Firefox, IE and Opera...
@@ -8,7 +9,26 @@
       bodyOffset > 0 ? navigation.classList.add('aw-nav-fixed') : navigation.classList.remove('aw-nav-fixed') // add or remove class
     }
   }
+
+  const onNavItemClick = () => {
+    const navItemList = document.querySelectorAll(".aw-section-link"); // all element with class aw-section-link
+    const navItems = [...navItemList];
+    navItems.forEach(item => {
+      item.addEventListener('click', event => {
+        event.preventDefault();
+        const sectionId = event.target.getAttribute('href') || event.target.dataset.href; // returns the element that triggered the event
+        scrollToSection(sectionId);
+      })
+    })
+  }
+
+  const scrollToSection = sectionId => {
+    console.log(sectionId)
+  }
+
   window.addEventListener("scroll", () => { // event fires when the document view or an element has been scrolled.
     addMenuBackground()
-  })
+  });
+
+  onNavItemClick();
 })();
